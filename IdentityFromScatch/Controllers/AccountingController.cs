ae4cf238035ea2cc219d355f796a7fd50b00ada5
Michael DaSilva <programmedmikey@gmail.com>
2015-10-14 16:59:31 -0400

    Creating a Identity application from scratch
    Creating a Identity application from scratch

---
+++ IdentityFromScatch/Controllers/AccountingController.cs
@@ -0,0 +1,28 @@
+﻿using IdentityFromScatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityFromScatch.Controllers
+{
+        [Authorize(Roles = "accounting, admin")]
+        public class AccountingController : Controller
+        {
+            // GET: Accounting
+            public ActionResult Index()
+            {
+            if (User.IsInRole(SecurityRoles.Admin))
+            {
+                return Content("welcome to accounting");
+            } 
+            else
+            {
+                return Content("get back to work");
+            }
+
+
+            }
+        }
+    }
